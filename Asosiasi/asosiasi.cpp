8227c1bd4280f838306f42e86beed3f00f040e40
yuixixixixi <yendra.paundra@gmail.com>
2025-05-27 11:21:21 +0700

    Refactor dokter and pasien classes to improve method organization and ensure proper function declarations

--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 
-class dokter:
+class dokter; // Forward declaration
 
 class pasien {  //asosiasi
     public:
@@ -33,12 +33,42 @@ class dokter {
 
     void tambahPasien(pasien*);
     void cetakPasien();
+};
+
+void dokter::tambahPasien(pasien* p) {
+    daftar_pasien.push_back(p);
+}
+
+void pasien::tambahDokter(dokter* d) {
+    daftar_dokter.push_back(d);
 }
 
 void dokter::cetakPasien() {
-    cout<< "Daftar pasien dari Dokter \"" <<this->nama << "\":\n"
+    cout << "Daftar pasien dari Dokter \"" << this->nama << "\":\n";
     for (auto& a : daftar_pasien) {
         cout << a->nama << "\n";
     }
     cout << endl;
+}
+void pasien::cetakDokter() {
+    cout << "Daftar dokter yang menangani Pasien \"" << this->nama << "\":\n";
+    for (auto& a : daftar_dokter) {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
+
+int main() {
+    dokter* varDokter1 = new dokter("dr. Budi");
+    dokter* varDokter2 = new dokter("dr. Tono");    
+    pasien* varPasien1 = new pasien("Andi");
+    pasien* varPasien2 = new pasien("Lia");
+
+    varDokter1->tambahPasien(varPasien1);
+    varDokter1->tambahPasien(varPasien2);
+    varDokter2->tambahPasien(varPasien1);
+
+    varPasien1->tambahDokter(varDokter1);
+    varPasien2->tambahDokter(varDokter1);
+    varPasien1->tambahDokter(varDokter2);
 }
