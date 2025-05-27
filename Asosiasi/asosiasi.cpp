bcccccb8c24d9b4104b2bdf871b6fd64b0a78833
yuixixixixi <yendra.paundra@gmail.com>
2025-05-27 11:13:10 +0700

    Refactor pasien and dokter classes to include destructor and methods for managing associated objects

--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -11,3 +11,34 @@ class pasien {  //asosiasi
     pasien(string pNama): nama(pNama) {
         cout << "Pasien \"" << nama << "\" ada\n";
     }
+    ~pasien() {
+        cout << "Pasien \"" <<  nama
+        << "\" tidak ada\n"; 
+    }
+
+    void tambahDokter(dokter*);
+    void cetakDokter();
+};
+
+class dokter {
+    public:
+    string nama;
+    vector<pasien*> daftar_pasien;
+    dokter(string pNama): nama(pNama) {
+        cout << "Dokter \"" << nama << "\" ada\n";
+    }
+    ~dokter() {
+        cout << "Dokter \"" << nama << "\" tidak ada\n";
+    }
+
+    void tambahPasien(pasien*);
+    void cetakPasien();
+}
+
+void dokter::cetakPasien() {
+    cout<< "Daftar pasien dari Dokter \"" <<this->nama << "\":\n"
+    for (auto& a : daftar_pasien) {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
